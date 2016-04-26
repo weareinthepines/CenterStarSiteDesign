8de7f5f842633da078d1a0064e71119faa2952f3
Thomas English <thomaswenglish@gmail.com>
2016-04-26 17:39:44 -0400

    newupdate

---
+++ index_files/vlb_engine/vlbdata1.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:true,borderSize:12,classNames:'vlightbox1',closeLocation:'top',descSliding:false,enableRightClick:false,enableSlideshow:true,prefix:'vlb1',resizeSpeed:7,slideTime:4,startZoom:true}) });
